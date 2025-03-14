adc907285522fca150eb3d042febddafc42c1a9b
Ilan Nowak--Glandier <inowak--@2C5.42angouleme.fr>
2025-03-14 18:31:58 +0100

    raccourci

---
+++ ex00/main.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: inowak-- <inowak--@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/14 13:11:08 by inowak--          #+#    #+#             */
+/*   Updated: 2025/03/14 15:00:01 by inowak--         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+int main(void)
+{
+    ClapTrap soldier1("soldier1");
+
+    std::cout << "----- start -----\n";
+    soldier1.attack("enemy");
+    soldier1.takeDamage(5);
+    soldier1.beRepaired(4);
+    std::cout << "----- end -----\n";
+}
